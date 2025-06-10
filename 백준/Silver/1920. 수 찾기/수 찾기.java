e899138cc5caea8b2aec2647f7fdefa66e6a34d1
yoonhyunjin02 <97629676+yoonhyunjin02@users.noreply.github.com>
2025-06-10 11:08:18 +0900

    [Silver IV] Title: 수 찾기, Time: 1708 ms, Memory: 192524 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                return 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        Arrays.sort(A);
+
+        int M = sc.nextInt();
+        
+        for (int i = 0; i < M; i++) {
+            int target = sc.nextInt();
+            System.out.println(binarySearch(A, target));
+        }
+    }
+}
