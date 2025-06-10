26448e2a1bdcafcecaaefa40e42c64da1a2c6800
yoonhyunjin02 <97629676+yoonhyunjin02@users.noreply.github.com>
2025-06-10 13:11:55 +0900

    [Silver II] Title: 랜선 자르기, Time: 164 ms, Memory: 17884 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] LAN = new int[K];
+        long max = 0;
+        
+        for (int i = 0; i < K; i++) {
+            LAN[i] = Integer.parseInt(br.readLine());
+            if (LAN[i] > max) {
+                max = LAN[i];
+            }
+        }
+
+        long start = 1;
+        long end = max;
+        long result = 0;
+
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long lines = 0;
+
+            for (int length : LAN) {
+                lines += (length / mid);
+            }
+
+            if (lines >= N) {
+                result = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
